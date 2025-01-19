48fcd3cccc656f429b8adf47eb9b08e74f2fd55e
OMG_OT <monsterroland7@gmail.com>
2025-01-19 19:03:26 +0100

    prealpha 0.0.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "294142174"
+    serviceWorkerVersion: "700302578"
   }
 });
